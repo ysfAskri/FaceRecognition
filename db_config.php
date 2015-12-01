c36e98d74fcd8305a47c4850325bc45c7ebc2239
root <ysf.askri@gmail.com>
2015-12-01 18:44:58 +0100

    first commit

---
+++ db_config.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * All database connection variables
+ */
+
+define('DB_USER', "root"); // db user
+define('DB_PASSWORD', "root"); // db password (mention your db password here)
+define('DB_DATABASE', "bd_face"); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
