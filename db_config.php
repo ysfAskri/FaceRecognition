6e7a4f91186b51dcecaf35816173055d8f1848bc
ysfAskri <ysf.askri@gmail.com>
2015-12-06 22:08:07 +0100

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -10,7 +10,7 @@
 //define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
 define('DB_DATABASE', "bd_face"); // database name
 
-define('DB_USER', "user"); // db user
-define('DB_PASSWORD', "user"); // db password (mention your db password here)
+define('DB_USER', "root"); // db user
+define('DB_PASSWORD', "root"); // db password (mention your db password here)
 define('DB_SERVER', "41.227.104.157"); // db server
 ?>
