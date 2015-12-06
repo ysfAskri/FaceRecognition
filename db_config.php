a49b489a189b3ac26c065885b06178bcd60fa5d3
ysfAskri <ysf.askri@gmail.com>
2015-12-06 20:21:10 +0100

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -4,8 +4,8 @@
  * All database connection variables
  */
 
-define('DB_USER', "root"); // db user
-define('DB_PASSWORD', "root"); // db password (mention your db password here)
+define('DB_USER', "adminWXD58tN"); // db user
+define('DB_PASSWORD', "NUmVuQ4VuBB5"); // db password (mention your db password here)
 define('DB_DATABASE', "bd_face"); // database name
-define('DB_SERVER', "localhost"); // db server
+define('DB_SERVER', "127.7.194.130"); // db server
 ?>
