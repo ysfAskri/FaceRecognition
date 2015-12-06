2d266c43b12f201db12727c5a48a1515dda88dbf
ysfAskri <ysf.askri@gmail.com>
2015-12-06 22:07:50 +0100

    Update db_config.php

--- db_config.php
+++ db_config.php
@@ -3,15 +3,14 @@
 /*
  * All database connection variables
  */
-define('DB_SERVER', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
-define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('DB_PASSWORD',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
+//define('DB_SERVER', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+//define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
+//define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+//define('DB_PASSWORD',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+//define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
 define('DB_DATABASE', "bd_face"); // database name
 
-//define('DB_USER', "adminWXD58tN"); // db user
-//define('DB_PASSWORD', "NUmVuQ4VuBB5"); // db password (mention your db password here)
-
-//define('DB_SERVER', "127.7.194.130"); // db server
+define('DB_USER', "user"); // db user
+define('DB_PASSWORD', "user"); // db password (mention your db password here)
+define('DB_SERVER', "41.227.104.157"); // db server
 ?>
